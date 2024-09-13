b8a1bfe7269438f71aa751fec3e09bf469b2d42d
Adam <adam_zafir_mohammed_sudhir@s2024.ssts.edu.sg>
2024-09-13 11:47:31 +0800

    Initial Commit

---
+++ Incoin Heist/Incoin_HeistApp.swift
@@ -0,0 +1,17 @@
+//
+//  Incoin_HeistApp.swift
+//  Incoin Heist
+//
+//  Created by Adam Zafir on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Incoin_HeistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
