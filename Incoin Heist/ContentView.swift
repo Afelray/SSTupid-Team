0c8f20801d286a02f279397165e1eaa0b0a3e053
Adam <adam_zafir_mohammed_sudhir@s2024.ssts.edu.sg>
2024-09-13 12:48:19 +0800

    test

--- Incoin Heist/ContentView.swift
+++ Incoin Heist/ContentView.swift
@@ -6,16 +6,45 @@
 //
 
 import SwiftUI
+import Lottie
+import SwiftPersistence
 
+// homescreen
 struct ContentView: View {
+    @Persistent("level") var level = 0
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hi, world!")
+        NavigationStack {
+            VStack {
+                HStack {
+                    Rectangle()
+                        .frame(width: 160, height: 170)
+                        .cornerRadius(10)
+                    
+                    NavigationLink(destination: Text("still blank -_-")) {
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 160, height: 170)
+                                .cornerRadius(10)
+                            
+                            VStack {
+                                Text("Level:")
+                                    .bold()
+                                    .fontWeight(.heavy)
+                                    .foregroundStyle(.black)
+                                Text("\(level)")
+                                    .bold()
+                                    .fontWeight(.heavy)
+                                    .foregroundStyle(.black)
+                                    .font(.system(size: 50))
+                            }
+                        }
+                    }
+                }
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
