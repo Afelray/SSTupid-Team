0519d610333847701fec9a47245eadb08cc2ed5e
Adam <adam_zafir_mohammed_sudhir@s2024.ssts.edu.sg>
2024-09-13 19:29:51 +0800

    Home screen Nav stuff

--- Incoin Heist/ContentView.swift
+++ Incoin Heist/ContentView.swift
@@ -6,48 +6,15 @@
 //
 
 import SwiftUI
-import Lottie
-import SwiftPersistence
 
-// homescreen
 struct ContentView: View {
-    @Persistent("level") var level = 0
-
     var body: some View {
         NavigationStack {
-            VStack {
-                HStack {
-                    Rectangle()
-                        .frame(width: 160, height: 170)
-                        .cornerRadius(10)
-                    
-                    NavigationLink(destination: Text("still blank -_-")) {
-                        ZStack {
-                            Rectangle()
-                                .frame(width: 160, height: 170)
-                                .cornerRadius(10)
-                            
-                            VStack {
-                                Text("Level:")
-                                    .bold()
-                                    .fontWeight(.heavy)
-                                    .foregroundStyle(.black)
-                                Text("\(level)")
-                                    .bold()
-                                    .fontWeight(.heavy)
-                                    .foregroundStyle(.black)
-                                    .font(.system(size: 50))
-                            }
-                        }
-                    }
-                }
-                Spacer()
-            }
-            .padding()
+            
         }
     }
 }
 
 #Preview {
-    ContentView()
+ContentView()
 }
---
+++ Incoin Heist/Home screen.swift
@@ -0,0 +1,55 @@
+//
+//  Home screen.swift
+//  Incoin Heist
+//
+//  Created by Adam Zafir on 9/13/24.
+//
+
+
+import SwiftUI
+import Lottie
+import SwiftPersistence
+
+// homescreen
+struct Home_screen: View {
+    @Persistent("level") var level = 0
+
+    var body: some View {
+        NavigationStack {
+            VStack {
+                HStack {
+                    Rectangle()
+                        .frame(width: 160, height: 170)
+                        .cornerRadius(10)
+                    
+                    NavigationLink(destination: Text("still blank -_-")) {
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 160, height: 170)
+                                .cornerRadius(10)
+                            
+                            VStack {
+                                Text("Level:")
+                                    .bold()
+                                    .fontWeight(.heavy)
+                                    .foregroundStyle(.black)
+                                Text("\(level)")
+                                    .bold()
+                                    .fontWeight(.heavy)
+                                    .foregroundStyle(.black)
+                                    .font(.system(size: 50))
+                            }
+                        }
+                    }
+                }
+                Spacer()
+            }
+            .padding()
+            .navigationTitle(Text("Home"))
+        }
+    }
+}
+
+#Preview {
+    Home_screen()
+}
