a9937964dde2c7311282b042084b20543aeba02e
Adam <adam_zafir_mohammed_sudhir@s2024.ssts.edu.sg>
2024-09-14 21:41:01 +0800

    Initial Commit

---
+++ INCoin HeistTests/INCoin_HeistTests.swift
@@ -0,0 +1,17 @@
+//
+//  INCoin_HeistTests.swift
+//  INCoin HeistTests
+//
+//  Created by Dev Account on 9/14/24.
+//
+
+import Testing
+@testable import INCoin_Heist
+
+struct INCoin_HeistTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
