775e45522921c8de25b11dbe0e8a9054ab2f1373
Adam <adam_zafir_mohammed_sudhir@s2024.ssts.edu.sg>
2024-09-15 19:34:27 +0800

    edited content view (not completed)

--- Incoin Heist/ContentView.swift
+++ Incoin Heist/ContentView.swift
@@ -6,16 +6,29 @@
 //
 
 import SwiftUI
-import Lottie
 struct ContentView: View {
+    @State var isanimated: Bool = false
     var body: some View {
         NavigationStack {
-            VStack {
-                
+                    ZStack {                        RoundedRectangle(cornerRadius: 30)
+                            .fill(.blue)
+                            .frame(height:  UIScreen.main.bounds.size.height)
+                            .edgesIgnoringSafeArea(.top)
+                        VStack {
+                            Button {
+                            } label : {
+                                Text("Lets Get Started")
+                                    .foregroundStyle(Color.red)
+                                    .fontWeight(.bold)
+                                    .font(.system(size: 60))
+                                    .padding()
+                                
+                            }
+                        }
+                    }
             }
         }
     }
-}
 
 #Preview {
 ContentView()
